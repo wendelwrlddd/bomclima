dc26ef83cfe95f45438c5ff2bab1c0d7e14f91ff
Wendel Monteiro <monteirowendel551@gmail.com>
2026-02-25 18:35:00 -0300

    Debug: Adicionando X-Debug-Version para confirmar deploy

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('X-Debug-Version', 'suprema-v1');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).send();
